f9c542ccb8ba432cf6ad8316c58b59f96f6f0b79
fanstefy <predolac_stefan@yahoo.com>
2025-03-17 21:48:05 +0100

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <div></div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+};
+
+export default App;
